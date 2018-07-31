b3da736fcae6359afd429c40a0527b310dbd9a4e
nicewahson <nicelj92@gmail.com>
2018-07-31 17:07:48 +0800

    配置

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,18 @@ module.exports = {
     chunkFilename: '[name].chunk.js'
   },
 
+  module:{
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+
   mode: 'development' // 相关配置 比如uglifyjs
 }
--- webpackfiles/index.webpack.js
+++ webpackfiles/index.webpack.js
@@ -1,12 +1,16 @@
 var requirePack1 = require('./index.webpack1');
-var _ = require('lodash');
+// var _ = require('lodash');
+require('./index.css');
 
 function component() {
   console.log(requirePack1);
 
   var element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], '~');
+  // element.innerHTML = _.join(['Hello', 'webpack'], '~');
+  element.innerHTML = 'hello world';
+
+  element.classList.add('red');
 
   return element;
 }
