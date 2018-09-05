ec2434d77f3fc1f97d6d25964c2e32ed493633d6
nicewahson <nicelj92@gmail.com>
2018-09-05 17:46:15 +0800

    测试babel 7

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackManifestPlugin = require('webpack-manifest-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: {
-    "page1/index": path.join(__dirname, 'webpackfiles/index.webpack.js'),
-    "page1/index1": path.join(__dirname, 'webpackfiles/index.webpack3.js'),
+    // "page1/index": path.join(__dirname, 'webpackfiles/index.webpack.js'),
+    // "page1/index1": path.join(__dirname, 'webpackfiles/index.webpack3.js'),
     // index1: path.join(__dirname, 'webpackfiles/index.webpack2.js'),
     // https://github.com/webpack/webpack.js.org/issues/970 chunk
     // chunk: 源代码的抽象，如何打包和如何组织代码的那部分代码，更像是一个连接的过程，通过chunkdist中
@@ -16,8 +16,8 @@ module.exports = {
     // 的代码，然后应该是initial chunk和normal chunk，也就是初始化时执行的代码，即传入的module数组中的
     // 每一项，一般就是initial chunk，而通过lazy load加载的一般就是normal chunk，也就是require.ensure、
     // import()等一些引入的chunk
-    // 'index-babel': [path.join(__dirname, 'webpackfiles/babel.index.js')]
-    "commonBundle": ["lodash"]
+    'index-babel': [path.join(__dirname, 'webpackfiles/babel.index.js')] // 不用preset env 398kb 用entry: 359kb 用usage: 87kb production 18kb
+    // "commonBundle": ["lodash"]
   },
   // devtool: 'inline-source-map',
 
@@ -37,10 +37,10 @@ module.exports = {
     new CleanWebpackPlugin([path.join(__dirname, 'build')]),
     new HtmlWebpackPlugin({
       title: 'htmlplugin',
-      filename: 'page1/index.html',
+      // filename: 'page1/index.html',
       // chunks: ['page1/index','page1/runtime1','page1/vendors']
     }),
-    new BundleAnalyzerPlugin()
+    // new BundleAnalyzerPlugin()
     // new WebpackManifestPlugin(),
     // new webpack.ProvidePlugin({
     //   _: 'lodash'
@@ -48,37 +48,37 @@ module.exports = {
     // new webpack.HotModuleReplacementPlugin()
   ],
 
-  optimization: {
-    // runtimeChunk: 'single', //会把webpack的runtime代码单独打包出来 剩余的也单独打包出来 包括css 变成chunkfile
-    runtimeChunk: {
-      name: 'page1/runtime1'
-    },
-    splitChunks: {
-      chunks: 'all',
-      // chunks(chunk){
-      //   console.log(chunk.name,'line====') // 出现undefined是async chunk 的name没有指定
-      //   return 'all'
-      // },
-      cacheGroups: {
-        commonUtil: {
-          chunks: 'initial',
-          name: 'page1/commonUtil',
-          minChunks: 2,
-          minSize: 200 // bytes 生成的chunk最小大小
-        },
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'page1/vendors', // 不使用name就用key值和chunk拼接
-          priority: 5  // groups中多项都命中某个module的话，按这个来，module存在优先级高的chunk里面 default:0
-          // chunks: 'all' // 和上面的chunks配置看起来没有区别(inherite来， 进行单独个性化配置)
-          // chunks(chunk){
-          //   console.log(chunk.name,'-----------')
-          // }
-        }
-      },
-      // automaticNameDelimiter : '-'
-    }
-  },
+  // optimization: {
+  //   // runtimeChunk: 'single', //会把webpack的runtime代码单独打包出来 剩余的也单独打包出来 包括css 变成chunkfile
+  //   runtimeChunk: {
+  //     name: 'page1/runtime1'
+  //   },
+  //   splitChunks: {
+  //     chunks: 'all',
+  //     // chunks(chunk){
+  //     //   console.log(chunk.name,'line====') // 出现undefined是async chunk 的name没有指定
+  //     //   return 'all'
+  //     // },
+  //     cacheGroups: {
+  //       commonUtil: {
+  //         chunks: 'initial',
+  //         name: 'page1/commonUtil',
+  //         minChunks: 2,
+  //         minSize: 200 // bytes 生成的chunk最小大小
+  //       },
+  //       vendor: {
+  //         test: /[\\/]node_modules[\\/]/,
+  //         name: 'page1/vendors', // 不使用name就用key值和chunk拼接
+  //         priority: 5  // groups中多项都命中某个module的话，按这个来，module存在优先级高的chunk里面 default:0
+  //         // chunks: 'all' // 和上面的chunks配置看起来没有区别(inherite来， 进行单独个性化配置)
+  //         // chunks(chunk){
+  //         //   console.log(chunk.name,'-----------')
+  //         // }
+  //       }
+  //     },
+  //     // automaticNameDelimiter : '-'
+  //   }
+  // },
 
   module:{
     rules: [
